3a829b3f2dec67420a1b0d85254bfe63676160dd
Joao Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2025-05-10 16:58:57 -0300

    Update go.mod and go.sum to include new dependencies; implement MQTT server in main.go and enhance Order struct in order.go

--- application/entities/order.go
+++ application/entities/order.go
@@ -1,3 +1,11 @@
 package entities
 
-type Order struct{}
+import "github.com/google/uuid"
+
+type Order struct {
+	id     uuid.UUID
+	code   string
+	userID uuid.UUID
+	locker int
+	status string
+}
--- main.go
+++ main.go
@@ -1,5 +1,49 @@
 package pegtech
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+	mqttserver "github.com/mochi-mqtt/server/v2"
+	auth "github.com/mochi-mqtt/server/v2/hooks/auth"
+	listeners "github.com/mochi-mqtt/server/v2/listeners"
+)
+
+func startMQTT() {
+	// Cria uma instância do broker MQTT
+	mqttServer := mqttserver.New(nil)
+
+	// Permite todas as conexões (apenas para desenvolvimento)
+	_ = mqttServer.AddHook(new(auth.AllowHook), nil)
+
+	// Cria um listener TCP na porta 1883
+	tcp := listeners.NewTCP(listeners.Config{ID: "t1", Address: ":1883"})
+	err := mqttServer.AddListener(tcp)
+	if err != nil {
+		log.Fatalf("Erro ao adicionar listener TCP: %v", err)
+	}
+
+	go func() {
+		log.Println("Iniciando servidor MQTT na porta :1883...")
+		err := mqttServer.Serve()
+		if err != nil {
+			log.Fatalf("Erro ao iniciar o servidor MQTT: %v", err)
+		}
+	}()
+}
+
+func main() {
+	// Inicia o servidor MQTT em uma goroutine
+	startMQTT()
+	// Create a new Echo instance
+	e := echo.New()
+
+	// Define a route
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome to PegTech API!")
+	})
+
+	// Start the server
+	e.Logger.Fatal(e.Start(":8080"))
 }
